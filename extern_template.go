58f5016f4ed493d80a424ff08cd88596a1a35fe6
GitUser2310 <gituser@smlsat.ru>
2019-09-24 13:25:58 +0300

    http serve in handler func

--- extern_template.go
+++ extern_template.go
@@ -12,35 +12,42 @@ type Todo struct {
 	Done bool
 }
 
+var todos = []Todo{
+	{"Выучить Go", false},
+	{"Посетить лекцию по Go", false},
+	{"...", false},
+	{"Profit", false},
+}
+
 func IsNotDone(todo Todo) bool {
 	return !todo.Done
 }
 
-func main() {
+type Handler struct {
+	http.Handler
+}
+
+func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	tmp1, err := template.New("template.html").Funcs(template.FuncMap{"IsNotDone": IsNotDone}).ParseFiles("template.html")
 	if err != nil {
 		log.Fatal("Can not expand template", err)
 		return
 	}
-	todos := []Todo{
-		{"Выучить Go", false},
-		{"Посетить лекцию по Go", false},
-		{"...", false},
-		{"Profit", false},
+
+	if r.Method == http.MethodPost {
+		param := r.FormValue("id")
+		index, _ := strconv.ParseInt(param, 10, 0)
+		todos[index].Done = true
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodPost {
-			param := r.FormValue("id")
-			index, _ := strconv.ParseInt(param, 10, 0)
-			todos[index].Done = true
-		}
+	err1 := tmp1.Execute(w, todos)
+	if err1 != nil {
+		http.Error(w, err1.Error(), http.StatusInternalServerError)
+	}
+}
 
-		err := tmp1.Execute(w, todos)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
-
+func main() {
+	handler := Handler{}
+	http.Handle("/", handler)
 	http.ListenAndServe(":8081", nil)
 }
